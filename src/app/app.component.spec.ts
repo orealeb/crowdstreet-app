8142c5ff654099d6e615bed464657f0f01c056f7
Ore <orealeb@yahoo.com>
2020-03-08 04:28:23 -0700

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TABLES } from '../app/mock-tables';
+import 'jest-extended';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule
       ],
       declarations: [
         AppComponent
@@ -26,10 +30,94 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('crowdstreet-app');
   });
 
-  it('should render title', () => {
+  it('should have table width text', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('crowdstreet-app app is running!');
+    expect(compiled.querySelector('.container span').textContent).toEqual('20%');
   });
+
+  it('should have configure button', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.container button').textContent).toEqual('Configure');
+  });
+
+  describe('Table Name', () => {
+    const expected = ['Table:', 'RED'];
+    it('should have table name', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement;
+      expect(compiled.querySelector('div.container.display_inline p').textContent.split(' ')).toEqual(expected);
+    });
+  });
+
+  test('records have all properties', () => {
+    const records = TABLES;
+    let numAsserts = 1;
+    records.forEach(record => {
+      expect(record).toHaveProperty('name');
+      expect.assertions(numAsserts++);  // true
+      expect(record).toHaveProperty('start');
+      expect.assertions(numAsserts++);  // true
+      expect(record).toHaveProperty('increment');
+      expect.assertions(numAsserts++);  // true
+      expect(record).toHaveProperty('width');
+      expect.assertions(numAsserts++);  // true
+      expect(record).toHaveProperty('selectedDir');
+      expect.assertions(numAsserts++);  // true
+      expect(record).toHaveProperty('directions');
+      expect.assertions(numAsserts++);  // true
+      expect(record).toHaveProperty('data');
+      expect.assertions(numAsserts++);  // true
+    });
+  })
+
+  
+  describe('Submit with default values', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+    const table = TABLES[0];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      component.submit(table.start, table.increment, table.max, table.width, table.selectedDir, table.name);
+    })
+
+    it('should create table with two rows and five columns with all zeroes', () => {
+      expect(component.currTable.data)
+      .toEqual([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]);
+    });
+
+    it('should have a width of 20%', () => {
+      expect(component.currTable.width)
+      .toBe(20);
+    });
+  });
+
+  describe('Submit with updated values', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+    const table = TABLES[0];
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      component.submit(3, 2, 18, 40, table.selectedDir, table.name);
+    })
+
+    it('should create table with start of 3, incremenent of 2, and max of 18', () => {
+      expect(component.currTable.data)
+      .toEqual([undefined, [13, 15, 17, undefined, undefined], [3, 5, 7, 9, 11]]);
+    });
+
+    it('should have a width of 40%', () => {
+      expect(component.currTable.width)
+      .toBe(40);
+    });
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { Table }    from'../app/table';
+import { TABLES } from '../app/mock-tables';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+
+  tables : Table[] = TABLES;
+  currTable : Table = this.tables[0];
+
   title = 'crowdstreet-app';
+  hideForm : boolean = true;
+
+
+
+  cancel(){
+    //this.resetFields();
+    this.hideForm = true;
+  }
+  showForm(name : string){
+    this.currTable = this.tables.find(table => {return  table.name == name})
+    this.hideForm = false;
+  }
+
+  submit(start: number, increment: number, max: number, width: number, dir: string, tableName : string) { 
+ 
+    let beginRow = 0;
+    let endRow = 0;
+    if(dir === 'LTR-UP'){
+      beginRow = Math.floor((max-start)/5) - 1 > 0? Math.floor((max-start)/5) - 1: 1; // since columns are predefined to 5, calculate number of rows
+      endRow = 0;
+    }
+    let table = new Array(beginRow);
+
+    for (let row = beginRow; row >= endRow && start <= max; row--) {
+      table[row] = new Array(5);
+      for(let col = 0;  col < 5 && start <= max; col++){
+        table[row][col] = start;
+        start += increment;
+      }
+    }
+    this.currTable.width = width;
+    this.currTable.data = table;
+  }
+
+
+
+  resetFields(table : Table){
+    table.start  = 0;
+    table.increment = 0;
+    table.max = 0;
+    table.width = 20;
+    table.selectedDir = table.directions[0];
+    }
+
 }
